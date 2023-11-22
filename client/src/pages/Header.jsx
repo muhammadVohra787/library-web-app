33c9dcc62aa23812cc25b7500a0bfe36f46c41ef
Bjornar Egede-Nissen <gallopping_gryphon@outlook.com>
2023-11-22 11:26:30 -0500

    Line ending CRLF -> LF

--- client/src/pages/Header.jsx
+++ client/src/pages/Header.jsx
@@ -1,27 +1,27 @@
-import React, { useState } from "react";
-import "bulma/css/bulma.min.css";
+import { useState } from 'react'
+import 'bulma/css/bulma.min.css'
 
 const Navbar = () => {
-    const [isActive, setIsActive] = useState(false);
+    const [ isActive, setIsActive ] = useState( false )
 
     const toggleNavbar = () => {
-        setIsActive(!isActive);
-    };
+        setIsActive( ! isActive )
+    }
 
     return (
         <nav className="navbar" role="navigation" aria-label="main navigation">
             <div className="navbar-brand">
                 <a className="navbar-item" href="/">
-                    <h3 style={{ color: "purple" }}>ETHEREAL</h3>
+                    <h3 style={ { color: 'purple' } }>ETHEREAL</h3>
                 </a>
 
                 <a
                     role="button"
-                    className={`navbar-burger burger ${isActive ? "is-active" : ""}`}
+                    className={ `navbar-burger burger ${isActive ? 'is-active' : ''}` }
                     aria-label="menu"
                     aria-expanded="false"
                     data-target="navbarBasicExample"
-                    onClick={toggleNavbar}
+                    onClick={ toggleNavbar }
                 >
                     <span aria-hidden="true"></span>
                     <span aria-hidden="true"></span>
@@ -29,7 +29,7 @@ const Navbar = () => {
                 </a>
             </div>
 
-            <div id="navbarBasicExample" className={`navbar-menu ${isActive ? "is-active" : ""}`}>
+            <div id="navbarBasicExample" className={ `navbar-menu ${isActive ? 'is-active' : ''}` }>
                 <div className="navbar-start">
                     <a className="navbar-item" href="/">
                         Home
@@ -54,7 +54,7 @@ const Navbar = () => {
                 </div>
             </div>
         </nav>
-    );
-};
+    )
+}
 
-export default Navbar;
+export default Navbar
