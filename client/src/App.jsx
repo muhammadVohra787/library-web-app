35a78fa071f6391131bfc620cf6ae69af7b936fb
Bjornar Egede-Nissen <gallopping_gryphon@outlook.com>
2023-11-22 11:30:56 -0500

    Indentation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,17 @@
-import { Container } from "@mui/material";
-import MainRouter from "./main-router";
-import Footer from "@/pages/Footer";
-import "./App.css";
-import Header from "@/pages/Header";
+import { Container } from '@mui/material'
+import MainRouter from './main-router'
+import Footer from '@/pages/Footer'
+import './App.css'
+import Header from '@/pages/Header'
 
 function App() {
     return (
         <Container>
             <Header />
-                <MainRouter />
+            <MainRouter />
             <Footer />
         </Container>
-    );
+    )
 }
 
-export default App;
+export default App
