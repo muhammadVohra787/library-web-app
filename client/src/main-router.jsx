e4baa9a0dc4c5a734465b875750bb82258b45375
Bjornar Egede-Nissen <gallopping_gryphon@outlook.com>
2023-11-23 13:53:29 -0500

    Dynamic book route

--- client/src/main-router.jsx
+++ client/src/main-router.jsx
@@ -13,7 +13,7 @@ const MainRouter = () => {
                 <Route path="/" element={ <Home /> } />
                 <Route path="/signin" element={ <SignIn /> } />
                 <Route path="/signup" element={ <SignUp /> } />
-                <Route path="/description" element={ <Book /> } />
+                <Route path="/book/:bookIdentifier" element={ <Book /> } />
                 <Route path="/explore" element={ <Explore /> } />
             </Routes>
         </BrowserRouter>
