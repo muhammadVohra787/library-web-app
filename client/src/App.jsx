aa3d5434afcbc3e23f9a20789b6c9059c761c21e
Bjornar Egede-Nissen <gallopping_gryphon@outlook.com>
2023-11-24 00:55:32 -0500

    Removed app.css and references

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import { Container } from '@mui/material'
 import MainRouter from './main-router'
 import Footer from '@/components/Footer'
-import './App.css'
 import Header from '@/components/Header'
 import Header2 from './components/Header2'
 import { Outlet } from 'react-router-dom'
