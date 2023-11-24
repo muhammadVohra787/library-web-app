52412323415039ba6d9eff46ba49b393ea24c5bc
Bjornar Egede-Nissen <gallopping_gryphon@outlook.com>
2023-11-24 11:27:57 -0500

    Removed obsolete code

--- client/src/main-router.jsx
+++ client/src/main-router.jsx
@@ -1,15 +1,10 @@
-import { BrowserRouter, Route, RouterProvider, Routes, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
-import Example from '@/routes/example'
+import { Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import Home from '@/pages/Home'
 import SignIn from '@/pages/SignIn.jsx'
 import SignUp from '@/pages/SignUp.jsx'
 import Explore from '@/pages/Explore.jsx'
 import Book from '@/pages/Book.jsx'
 import App from './App'
-
-function Root( ) {
-    return <p>xyz</p>
-}
 
 const router = createBrowserRouter(
     createRoutesFromElements(
@@ -22,19 +17,5 @@ const router = createBrowserRouter(
         </Route>,
     ),
 )
-
-const MainRouter2 = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={ <Home /> } />
-                <Route path="/signin" element={ <SignIn /> } />
-                <Route path="/signup" element={ <SignUp /> } />
-                <Route path="/book/:bookIdentifier" element={ <Book /> } />
-                <Route path="/explore" element={ <Explore /> } />
-            </Routes>
-        </BrowserRouter>
-    )
-}
 
 export default router
