ebb7f877e412dab99e428c218542cdc714b87393
Julian0718 <julian.rodriguez0718@gmail.com>
2023-11-24 23:54:45 -0800

    feat: Add User model and user routes

--- server.js
+++ server.js
@@ -1,27 +1,11 @@
 /**
  * App Entrypoint
  */
-import mongoose from "mongoose";
 import config from "./server/config/config.js";
+import { connectDB } from "./server/db.js";
 import app from "./server/express.js";
 
-mongoose.Promise = global.Promise;
-
-mongoose
-    .connect(config.mongoUri, {
-        useNewUrlParser: true,
-        // useCreateIndex: true,
-        useUnifiedTopology: true,
-    })
-    .then(() => {
-        console.log("Connected to the database!");
-    });
-
-mongoose.connection.on("error", () => {
-    console.log(config.mongoUri);
-    throw new Error("unable to connect to database: ${config.mongoUri}");
-});
-
+connectDB();
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to this application." });
 });
