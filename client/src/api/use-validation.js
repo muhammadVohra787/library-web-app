2253fedac71d6ddb1c22fc4fa765a9cfc57e43f6
Bjornar Egede-Nissen <gallopping_gryphon@outlook.com>
2023-11-26 15:08:58 -0500

    Added

---
+++ client/src/api/use-validation.js
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+
+export default function useValidation() {
+    /** @type [any, (prev: any) => void] */
+    const [ errors, setErrors ] = useState( {} )
+
+    const updateErrors = ( field, error ) => {
+        setErrors( ( prev ) => {
+            return {
+                ...prev,
+                [ field ]: error,
+            }
+        } )
+    }
+    return {
+        get errors() {
+            return errors
+        },
+        validate( field, value ) {
+            if ( field === 'email' ) {
+                const emailPattern =
+                        /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$/
+
+                const isValid = emailPattern.test( value )
+                updateErrors( 'email', isValid ? undefined : 'Invalid email address.' )
+                return isValid
+            }
+
+            if ( field === 'name' ) {
+                const isValid = value.length >= 1
+                updateErrors( 'name', isValid ? undefined : 'Name cannot be empty.' )
+                return isValid
+            }
+
+            // if ( field === 'password' ) {
+            // 'Password must have at least 8 characters'
+            // }
+
+            return { isValid: true, errorMessage: '' }
+        },
+    }
+}
