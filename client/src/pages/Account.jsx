429e1bd873db36f9bef157a85f2ceb4830be3519
Bjornar Egede-Nissen <gallopping_gryphon@outlook.com>
2023-11-26 15:22:01 -0500

    Enabled user editing

--- client/src/pages/Account.jsx
+++ client/src/pages/Account.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
 import {
     Box,
     Container,
@@ -15,89 +15,171 @@ import {
     FormControlLabel,
     InputLabel,
     FormHelperText,
+    Alert,
+    CircularProgress,
 } from '@mui/material'
+import useAuthentication from '@/api/use-authentication'
+import Authorized from '@/components/Authorized'
+import useAccount from '@/api/use-account'
+import useValidation from '@/api/use-validation'
+
 export default function Account() {
-    const [ name, setName ] = useState()
+    const user = useAuthentication()
+    const account = useAccount()
+    const { validate, errors } = useValidation()
 
-    const [ email, setEmail ] = useState()
-
-    const [ password, setPassword ] = useState()
+    const [ name, setName ] = useState( '' )
+    const [ email, setEmail ] = useState( '' )
+    const [ password, setPassword ] = useState( '' )
 
     const resetHandler = () => {
-
+        setName( user.userData.name )
+        setEmail( user.userData.email )
+        // setPassword( user.userData.password )
     }
-    const saveHandler = ( ) =>{
 
+    const saveHandler = ( e ) =>{
+        e.preventDefault()
+        const nameValidation = validate( 'name', email )
+        const emailValidation = validate( 'email', email )
+        console.log( user )
+
+        if ( emailValidation && nameValidation ) {
+            account.updateUser( user.userId, { name, email } )
+        }
     }
 
     const onNameChange = ( event ) => {
         setName( event.target.value )
+        validate( 'name', event.target.value )
     }
 
     const onEmailChange = ( event ) => {
         setEmail( event.target.value )
+        validate( 'email', event.target.value )
     }
 
     const onPasswordChange = ( event ) =>{
-        setPassword( event.target.value )
+        // setPassword( event.target.value )
     }
+
+    useEffect( () => {
+        console.log( 'ACCOUNT ==> ', user )
+
+        if ( user.userData ) {
+            resetHandler()
+        }
+    }, [ user.isGettingStatus ] )
+
+    useEffect( () => {
+        console.log( 'account.status.isComplete ==> ', user )
+
+        if ( account.status.isComplete ) {
+            user.refresh()
+        }
+    }, [ account.status.isComplete ] )
 
     return (
         <Container style={ { marginTop: '20px' } }>
-            <Grid container spacing={ 3 }>
-                <Grid item xs={ 12 } md={ 6 }>
-                    <Box p={ 5 }>
-                        <Typography variant="h4" gutterBottom>
-                            User Account Detail
-                        </Typography>
+            <Stack direction="row" justifyContent="space-between" alignItems="center" mb={ 6 }>
+                <Typography variant="h4" component="h2">
+                    User Account
+                </Typography>
+                {
+                    user.isSignedIn && <Button variant="outlined" onClick={ () => user.signOut() }>Sign out</Button>
+                }
+            </Stack>
 
-                        <Grid container spacing={ 3 } direction="row">
-                            <Grid item xs={ 12 } md={ 4 }>
-                                <InputLabel>
-                                    Name
-                                </InputLabel>
-                            </Grid>
-                            <Grid item xs={ 12 } md={ 8 }>
-                                <TextField id="name" fullWidth required value={ name } onChange={ onNameChange } />
-                                <FormHelperText>(Enter Full Name)</FormHelperText>
-                            </Grid>
+            <Authorized>
+                <Grid container spacing={ 10 }>
+                    <Grid item xs={ 12 } md={ 6 }>
+                        <Paper>
+                            <Box p={ 5 }>
+                                <Typography variant="h5" component="h3" mb={ 4 }>
+                                    Account Details
+                                </Typography>
 
-                            <Grid item xs={ 12 } md={ 4 }>
-                                <InputLabel>
-                                    Email
-                                </InputLabel>
-                            </Grid>
-                            <Grid item xs={ 12 } md={ 8 }>
-                                <TextField id="email" fullWidth required value ={ email } onChange={ onEmailChange } />
-                                <FormHelperText>(Enter Email address)</FormHelperText>
-                            </Grid>
+                                <Grid container spacing={ 3 } direction="row">
+                                    <Grid item xs={ 12 } md={ 4 }>
+                                        <InputLabel>
+                                            Name
+                                        </InputLabel>
+                                    </Grid>
+                                    <Grid item xs={ 12 } md={ 8 }>
+                                        <TextField
+                                            id="name"
+                                            fullWidth
+                                            required
+                                            value={ name }
+                                            onChange={ onNameChange }
+                                            error={ !! errors.name }
+                                            helperText={ errors.name }
+                                        />
+                                        <FormHelperText>(Enter Full Name)</FormHelperText>
+                                    </Grid>
 
-                            <Grid item xs={ 12 } md={ 4 }>
-                                <InputLabel>
-                                    Password
-                                </InputLabel>
-                            </Grid>
-                            <Grid item xs={ 12 } md={ 8 }>
-                                <TextField id="password" fullWidth required value={ password } onChange={ onPasswordChange } />
-                                <FormHelperText>(Enter password)</FormHelperText>
-                            </Grid>
-                        </Grid>
-                        <Stack direction="row" spacing={ 3 }>
-                            <Button variant='contained' onClick={ saveHandler }>Save </Button>
-                            <Button onClick={ resetHandler }> Reset </Button>
-                        </Stack>
+                                    <Grid item xs={ 12 } md={ 4 }>
+                                        <InputLabel>
+                                            Email
+                                        </InputLabel>
+                                    </Grid>
+                                    <Grid item xs={ 12 } md={ 8 }>
+                                        <TextField
+                                            id="email"
+                                            fullWidth
+                                            required
+                                            value={ email }
+                                            onChange={ onEmailChange }
+                                            error={ !! errors.email }
+                                            helperText={ errors.email }
+                                        />
+                                        <FormHelperText>(Enter email address)</FormHelperText>
+                                    </Grid>
 
-                    </Box>
+                                    <Grid item xs={ 12 } md={ 4 }>
+                                        <InputLabel>
+                                            Password
+                                        </InputLabel>
+                                    </Grid>
+                                    <Grid item xs={ 12 } md={ 8 }>
+                                        <TextField id="password" fullWidth required value={ password } onChange={ onPasswordChange } disabled />
+                                        <FormHelperText>(Enter new password -- not implemented)</FormHelperText>
+                                    </Grid>
+                                </Grid>
+                                <Stack mt={ 6 } spacing={ 3 }>
+                                    {
+                                        account.status.isFetching && <Box sx={ { display: 'flex' } }>
+                                            <CircularProgress />
+                                        </Box>
+                                    }
+                                    {
+                                        account.status.isComplete &&
+                                            <Alert severity="success">
+                                                <Typography component="h2">Changes have been saved.</Typography>
+                                            </Alert>
+                                    }
+                                    {
+                                        ! account.status.isFetching &&
+                                            <Stack direction="row" spacing={ 3 }>
+                                                <Button variant='contained' onClick={ saveHandler }>Save </Button>
+                                                <Button onClick={ resetHandler }> Reset </Button>
+                                            </Stack>
+                                    }
+                                </Stack>
+                            </Box>
+                        </Paper>
+                    </Grid>
+                    <Grid item xs={ 12 } md={ 6 }>
+                        <Paper>
+                            <Box p={ 5 }>
+                                <Typography variant="h5" component="h3" mb={ 4 }>
+                                    Checked-out Books
+                                </Typography>
+                            </Box>
+                        </Paper>
+                    </Grid>
                 </Grid>
-                <Grid item xs={ 12 } md={ 6 }>
-                    <Paper style={ { padding: '20px', borderRadius: '10px' } }>
-                        <Typography variant="h4" gutterBottom>
-                            checkout books
-                        </Typography>
-
-                    </Paper>
-                </Grid>
-            </Grid>
+            </Authorized>
         </Container>
     )
 }
