663e0b1ebfbae4d5704239a45dbcd3e350ec34ec
Bjornar Egede-Nissen <gallopping_gryphon@outlook.com>
2023-11-26 00:03:00 -0500

    Removed theme-provider

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -15,7 +15,6 @@ import { useContext, useRef, useState } from 'react'
 import useAccount from '../api/use-account'
 import useAuthentication from '../api/use-authentication'
 import authContext from '../api/auth-context'
-const defaultTheme = createTheme()
 
 export default function SignUp() {
     const letters = /^[A-Za-z\s]+$/
@@ -104,88 +103,86 @@ export default function SignUp() {
     }
 
     return (
-        <ThemeProvider theme={ defaultTheme }>
-            <Container component="main" maxWidth="xs">
-                <CssBaseline />
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <Box
+                sx={ {
+                    marginTop: 8,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                } }
+            >
+                <Avatar sx={ { m: 1, bgcolor: 'secondary.main' } }>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Sign up
+                </Typography>
                 <Box
-                    sx={ {
-                        marginTop: 8,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                    } }
+                    component="form"
+                    noValidate
+                    onSubmit={ handleSubmit }
+                    sx={ { mt: 3 } }
                 >
-                    <Avatar sx={ { m: 1, bgcolor: 'secondary.main' } }>
-                        <LockOutlinedIcon />
-                    </Avatar>
-                    <Typography component="h1" variant="h5">
-                        Sign up
-                    </Typography>
-                    <Box
-                        component="form"
-                        noValidate
-                        onSubmit={ handleSubmit }
-                        sx={ { mt: 3 } }
+                    <Grid container spacing={ 2 }>
+                        <Grid item xs={ 12 }>
+                            <TextField
+                                required
+                                fullWidth
+                                id="name"
+                                label="Name"
+                                name="name"
+                                autoComplete="name"
+                                value={ formData.name }
+                                onChange={ handleChange }
+                                error={ !! errors.name }
+                                helperText={ errors.name }
+                            />
+                        </Grid>
+                        <Grid item xs={ 12 }>
+                            <TextField
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                autoComplete="email"
+                                value={ formData.email }
+                                onChange={ handleChange }
+                                error={ !! errors.email }
+                                helperText={ errors.email }
+                            />
+                        </Grid>
+                        <Grid item xs={ 12 }>
+                            <TextField
+                                fullWidth
+                                name="password"
+                                label="Password (not implemented)"
+                                type="password"
+                                id="password"
+                                autoComplete="new-password"
+                            />
+                        </Grid>
+                        <Grid item xs={ 12 }></Grid>
+                    </Grid>
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        sx={ { mt: 3, mb: 2 } }
                     >
-                        <Grid container spacing={ 2 }>
-                            <Grid item xs={ 12 }>
-                                <TextField
-                                    required
-                                    fullWidth
-                                    id="name"
-                                    label="Name"
-                                    name="name"
-                                    autoComplete="name"
-                                    value={ formData.name }
-                                    onChange={ handleChange }
-                                    error={ !! errors.name }
-                                    helperText={ errors.name }
-                                />
-                            </Grid>
-                            <Grid item xs={ 12 }>
-                                <TextField
-                                    required
-                                    fullWidth
-                                    id="email"
-                                    label="Email Address"
-                                    name="email"
-                                    autoComplete="email"
-                                    value={ formData.email }
-                                    onChange={ handleChange }
-                                    error={ !! errors.email }
-                                    helperText={ errors.email }
-                                />
-                            </Grid>
-                            <Grid item xs={ 12 }>
-                                <TextField
-                                    fullWidth
-                                    name="password"
-                                    label="Password (not implemented)"
-                                    type="password"
-                                    id="password"
-                                    autoComplete="new-password"
-                                />
-                            </Grid>
-                            <Grid item xs={ 12 }></Grid>
+                        Sign Up
+                    </Button>
+                    <Grid container justifyContent="flex-end">
+                        <Grid item>
+                            <Link href="/signin" variant="body2">
+                                Already have an account? Sign in
+                            </Link>
                         </Grid>
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            sx={ { mt: 3, mb: 2 } }
-                        >
-                            Sign Up
-                        </Button>
-                        <Grid container justifyContent="flex-end">
-                            <Grid item>
-                                <Link href="/signin" variant="body2">
-                                    Already have an account? Sign in
-                                </Link>
-                            </Grid>
-                        </Grid>
-                    </Box>
+                    </Grid>
                 </Box>
-            </Container>
-        </ThemeProvider>
+            </Box>
+        </Container>
     )
 }
