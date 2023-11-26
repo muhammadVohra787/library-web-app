4f399fc1e7a190ff6658e1cff28891d80d2ef849
Bjornar Egede-Nissen <gallopping_gryphon@outlook.com>
2023-11-26 16:39:00 -0500

    Fixed errors

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,71 +1,76 @@
-import React from "react";
+import React from 'react'
 import {
     Menu,
     MenuItem,
     IconButton,
+    Stack,
     Typography,
     useMediaQuery,
-} from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
-import { Link, Stack } from "@mui/material";
-import NavLink from "./NavLink";
-import { useAuth } from "../api/auth-context";
+    Link,
+    Box,
+} from '@mui/material'
+import MenuIcon from '@mui/icons-material/Menu'
+// import { Link, Stack } from '@mui/material'
+import NavLink from './NavLink'
+import useAuthentication from '@/api/use-authentication'
 
 export function NavBar() {
-    const [anchorEl, setAnchorEl] = React.useState(null);
-    const { isLoggedIn, logout } = useAuth();
-    const isMobile = useMediaQuery("(max-width:1000px)");
+    const [ anchorEl, setAnchorEl ] = React.useState( null )
+    const isMobile = useMediaQuery( '(max-width:1000px)' )
+    const auth = useAuthentication()
 
-    const handleClick = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
+    const handleClick = ( event ) => {
+        setAnchorEl( event.currentTarget )
+    }
 
     const handleClose = () => {
-        setAnchorEl(null);
-    };
+        setAnchorEl( null )
+    }
 
     const isActive =
         window.location.pathname === window.location.pathname
-            ? "is-active"
-            : "";
+            ? 'is-active'
+            : ''
 
     return (
         <Stack
             component="nav"
             className="main-menu"
             direction="row"
-            justifyContent={isMobile ? "flex-end" : "space-between"}
+            justifyContent={ isMobile ? 'flex-end' : 'space-between' }
             flexGrow="1"
+            width="100%"
+
         >
-            {isMobile ? (
+            { isMobile ? (
                 <>
                     <IconButton
                         aria-label="menu"
                         aria-controls="simple-menu"
                         aria-haspopup="true"
-                        onClick={handleClick}
+                        onClick={ handleClick }
                     >
                         <MenuIcon />
                     </IconButton>
                     <Menu
                         id="simple-menu"
-                        anchorEl={anchorEl}
+                        anchorEl={ anchorEl }
                         keepMounted
-                        open={Boolean(anchorEl)}
-                        onClose={handleClose}
+                        open={ Boolean( anchorEl ) }
+                        onClose={ handleClose }
                     >
-                        {/* Existing menu items */}
-                        <MenuItem onClick={handleClose}>
+                        { /* Existing menu items */ }
+                        <MenuItem onClick={ handleClose }>
                             <NavLink href="/">Home</NavLink>
                         </MenuItem>
-                        <MenuItem onClick={handleClose}>
+                        <MenuItem onClick={ handleClose }>
                             <NavLink href="/explore">Explore Books</NavLink>
                         </MenuItem>
-                        <MenuItem onClick={handleClose}>
+                        <MenuItem onClick={ handleClose }>
                             <NavLink href="/account">My Account</NavLink>
                         </MenuItem>
-                        {/* Added the Sign in/out link to the dropdown menu */}
-                        <MenuItem onClick={handleClose}>
+                        { /* Added the Sign in/out link to the dropdown menu */ }
+                        <MenuItem onClick={ handleClose }>
                             <Link href="/signin" variant="body2">
                                 Sign in
                             </Link>
@@ -75,35 +80,30 @@ export function NavBar() {
             ) : (
                 <Stack
                     direction="row"
-                    spacing={5}
+                    spacing={ 5 }
                     alignItems="center"
+                    justifyContent="space-between"
                     flexGrow="1"
                 >
-                    {/* Existing navigation links */}
-                    <NavLink href="/">Home</NavLink>
-                    <NavLink href="/explore">Explore Books</NavLink>
-                    {isLoggedIn ? (
-                        <Link href="/profile" variant="body2">
-                            My Account
-                        </Link>
-                    ) : (
-                        <Link href="/signin" variant="body2">
-                            My Account
-                        </Link>
-                    )}
-                    {/* Added the Sign in/out link outside the dropdown for larger screens */}
-                    {isLoggedIn ? (
-                        <Link href="/signin" variant="body2">
-                            Sign In
-                        </Link>
-                    ) : (
-                        <Link href="/signin" variant="body2">
-                            Sign Out
-                        </Link>
-                    )}
+                    <Stack
+                        direction="row"
+                        spacing={ 5 }
+                        alignItems="center"
+                    >
+                        <NavLink to="/">Home</NavLink>
+                        <NavLink to="/explore">Explore Books</NavLink>
+                        <NavLink to="/account">My Account</NavLink>
+                    </Stack>
+                    <Box alignSelf="flex-end">
+                        {
+                            auth.isSignedIn
+                                ? <NavLink asButton variant="outlined" onClick={ () => auth.signOut() } sx={ { fontSize: '0.9em !important' } }>Sign out</NavLink>
+                                : <NavLink asButton variant="outlined" to="/signin" sx={ { fontSize: '0.9em !important' } }>Sign In</NavLink>
+                        }
+                    </Box>
                 </Stack>
-            )}
-            {/* Removed the separate stack for the Sign in/out link for mobile view */}
+            ) }
+
         </Stack>
-    );
+    )
 }
