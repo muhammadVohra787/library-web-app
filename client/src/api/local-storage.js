33abf6b79c355def801937af6ca8db4272e782ed
Bjornar Egede-Nissen <gallopping_gryphon@outlook.com>
2023-11-26 15:08:49 -0500

    Added

---
+++ client/src/api/local-storage.js
@@ -0,0 +1,30 @@
+import { useState, useEffect, useRef } from 'react'
+
+function getStorageValue( key, defaultValue ) {
+    const saved = localStorage.getItem( key )
+
+    try {
+        const initial = saved ? JSON.parse( saved ) : null
+        return initial || defaultValue
+    }
+    catch ( e ) {
+        return defaultValue
+    }
+}
+
+export const useLocalStorage = ( key, defaultValue ) => {
+    const isMounted = useRef( false )
+    const [ value, setValue ] = useState( () => {
+        return getStorageValue( key, defaultValue )
+    } )
+
+    useEffect( () => {
+        if ( value !== defaultValue && value !== undefined ) {
+            console.log( 'localStorage.setItem', value )
+            localStorage.setItem( key, JSON.stringify( value ) )
+        }
+        isMounted.current = true
+    }, [ key, value ] )
+
+    return [ value, setValue ]
+}
