7ae5d24fa6db4464907cc3ddb2174b62a7c6aa75
Bjornar Egede-Nissen <gallopping_gryphon@outlook.com>
2023-11-26 16:39:12 -0500

    Added missing auth context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,23 @@
-import { Container } from "@mui/material";
-import MainRouter from "./main-router";
-import Footer from "@/components/Footer";
-import Header from "./components/Header";
-import { Outlet } from "react-router-dom";
-import { AuthProvider } from "./api/auth-context";
+import { Container } from '@mui/material'
+import MainRouter from './main-router'
+import Footer from '@/components/Footer'
+import Header from './components/Header'
+import { Outlet } from 'react-router-dom'
+import { AuthContextProvider } from './components/AuthContextProvider'
+// import { AuthProvider } from "./api/auth-context";
 
 function App() {
     return (
         <Container>
-            <AuthProvider>
+            <AuthContextProvider>
                 <Header />
                 <Container component="main" maxWidth="xl">
                     <Outlet />
                 </Container>
                 <Footer />
-            </AuthProvider>
+            </AuthContextProvider>
         </Container>
-    );
+    )
 }
 
-export default App;
+export default App
