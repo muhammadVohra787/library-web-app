28b3d0bd25e7fdeb78c1b23591a79d1f922bd71e
Bjornar Egede-Nissen <gallopping_gryphon@outlook.com>
2023-11-26 16:05:10 -0500

    Added button style

--- client/src/components/NavLink.jsx
+++ client/src/components/NavLink.jsx
@@ -1,15 +1,22 @@
 import { Link as RouterLink, useLocation } from 'react-router-dom'
-import { Link } from '@mui/material'
+import { Button, Link } from '@mui/material'
 
+/**
+ * Open links without reloading page.
+ *
+ * @param {*} props
+ */
 export default function NavLink( props ) {
-    const { href = undefined, ...rest } = props
+    const { href = undefined, asButton = false, ...rest } = props
     const location = useLocation()
-
     rest.to = href ? href : rest.to
+
+    if ( asButton ) {
+        return <Button { ...rest } component={ RouterLink } />
+    }
+
     const isActive = location.pathname === rest.to ? 'is-active' : ''
     rest.className += ` ${ isActive}`
 
-    return (
-        <Link { ...rest } component={ RouterLink } />
-    )
+    return <Link { ...rest } component={ RouterLink } />
 }
