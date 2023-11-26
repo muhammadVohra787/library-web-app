27a08dd6c1d489ba91770d15c6bfe057da13f51d
Bjornar Egede-Nissen <gallopping_gryphon@outlook.com>
2023-11-26 15:22:29 -0500

    Enabled sign in

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,3 +1,6 @@
+import { useContext, useEffect, useState } from 'react'
+// import { Redirect } from 'react-router-dom'
+
 import Avatar from '@mui/material/Avatar'
 import Button from '@mui/material/Button'
 import CssBaseline from '@mui/material/CssBaseline'
@@ -10,41 +13,18 @@ import Box from '@mui/material/Box'
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
 import Typography from '@mui/material/Typography'
 import Container from '@mui/material/Container'
-import { createTheme, ThemeProvider } from '@mui/material/styles'
-import { useState } from 'react'
-
-const defaultTheme = createTheme()
+import useAuthentication from '../api/use-authentication'
+import { Alert, CircularProgress, Stack } from '@mui/material'
+import useValidation from '@/api/use-validation'
 
 export default function SignIn() {
     const [ formData, setFormData ] = useState( {
         email: '',
-        password: '',
+        // password: '',
     } )
 
-    const [ errors, setErrors ] = useState( {
-        email: '',
-        password: '',
-    } )
-
-    const validateInput = ( name, value ) => {
-        if ( name === 'email' ) {
-            const emailPattern =
-                /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$/
-            return {
-                isValid: emailPattern.test( value ),
-                errorMessage: 'Invalid email address',
-            }
-        }
-
-        if ( name === 'password' ) {
-            return {
-                isValid: value.length >= 8,
-                errorMessage: 'Password must have at least 8 characters',
-            }
-        }
-
-        return { isValid: true, errorMessage: '' }
-    }
+    const auth = useAuthentication()
+    const { validate, errors } = useValidation()
 
     const handleChange = ( e ) => {
         const { name, value } = e.target
@@ -53,117 +33,136 @@ export default function SignIn() {
             [ name ]: value,
         } ) )
 
-        const { isValid, errorMessage } = validateInput( name, value )
-        setErrors( ( prevErrors ) => ( {
-            ...prevErrors,
-            [ name ]: isValid ? '' : errorMessage,
-        } ) )
+        // const { isValid, errorMessage } =
+        validate( name, value )
+        // setErrors( ( prevErrors ) => ( {
+        //     ...prevErrors,
+        //     [ name ]: isValid ? '' : errorMessage,
+        // } ) )
     }
 
     const handleSubmit = ( event ) => {
         event.preventDefault()
 
-        const { email, password } = formData
+        const { email } = formData
 
-        const emailValidation = validateInput( 'email', email )
-        const passwordValidation = validateInput( 'password', password )
+        const emailValidation = validate( 'email', email )
+        // const passwordValidation = auth.validate( 'password', password )
 
-        setErrors( {
-            email: emailValidation.isValid ? '' : emailValidation.errorMessage,
-            password: passwordValidation.isValid
-                ? ''
-                : passwordValidation.errorMessage,
-        } )
+        // setErrors( {
+        //     email: emailValidation.isValid ? '' : emailValidation.errorMessage,
+        //     // password: passwordValidation.isValid
+        //     //     ? ''
+        //     //     : passwordValidation.errorMessage,
+        // } )
 
-        if ( emailValidation.isValid && passwordValidation.isValid ) {
-            console.log( {
-                email,
-                password,
-            } )
+        if ( emailValidation ) {
+            // !! Note - no authentication at this time
+            // Just a check if the email exists
+            auth.signIn( email )
         }
     }
 
     return (
-        <ThemeProvider theme={ defaultTheme }>
-            <Container component="main" maxWidth="xs">
-                <CssBaseline />
-                <Box
-                    sx={ {
-                        marginTop: 8,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                    } }
-                >
-                    <Avatar sx={ { m: 1, bgcolor: 'secondary.main' } }>
-                        <LockOutlinedIcon />
-                    </Avatar>
-                    <Typography component="h1" variant="h5">
-                        Sign in
-                    </Typography>
-                    <Box
-                        component="form"
-                        onSubmit={ handleSubmit }
-                        noValidate
-                        sx={ { mt: 1 } }
-                    >
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            id="email"
-                            label="Email Address"
-                            name="email"
-                            autoComplete="email"
-                            autoFocus
-                            value={ formData.email }
-                            onChange={ handleChange }
-                            error={ !! errors.email }
-                            helperText={ errors.email }
-                        />
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            name="password"
-                            label="Password"
-                            type="password"
-                            id="password"
-                            autoComplete="current-password"
-                            value={ formData.password }
-                            onChange={ handleChange }
-                            error={ !! errors.password }
-                            helperText={ errors.password }
-                        />
-                        <FormControlLabel
-                            control={
-                                <Checkbox value="remember" color="primary" />
-                            }
-                            label="Remember me"
-                        />
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            sx={ { mt: 3, mb: 2 } }
-                        >
-                            Sign In
-                        </Button>
-                        <Grid container>
-                            <Grid item xs>
-                                <Link href="#" variant="body2">
-                                    Forgot password?
-                                </Link>
-                            </Grid>
-                            <Grid item>
-                                <Link href="/signup" variant="body2">
-                                    Don't have an account? Sign Up
-                                </Link>
-                            </Grid>
-                        </Grid>
-                    </Box>
+        <Container component="main" maxWidth="xs">
+            {
+                auth.isGettingStatus && <Box sx={ { display: 'flex' } }>
+                    <CircularProgress />
                 </Box>
-            </Container>
-        </ThemeProvider>
+            }
+            {
+                auth.isSignedIn && <Stack spacing={ 3 } mt={ 10 } mb={ 10 } alignItems="stretch">
+                    <Alert severity="success">
+                        <Typography component="h2">Signed in</Typography>
+                    </Alert>
+                    <Stack direction="row" justifyContent="center" spacing={ 3 }>
+                        <Button variant="contained" href="/">Front Page</Button>
+                        <Button variant="contained" href="/account">My Account</Button>
+                    </Stack>
+                </Stack>
+            }
+            {
+                ! auth.isGettingStatus && ! auth.isSignedIn &&
+                    <Box
+                        sx={ {
+                            marginTop: 8,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                        } }
+                    >
+                        <Avatar sx={ { m: 1, bgcolor: 'secondary.main' } }>
+                            <LockOutlinedIcon />
+                        </Avatar>
+                        <Typography component="h1" variant="h5">
+                            Sign in
+                        </Typography>
+                        {
+                            auth.isSignInFailed && <Stack spacing={ 3 } mt={ 4 } mb={ 4 } alignItems="stretch">
+                                <Alert severity="error">
+                                    <Typography component="h2">Wrong email or password.</Typography>
+                                </Alert>
+                            </Stack>
+                        }
+                        <Box
+                            component="form"
+                            onSubmit={ handleSubmit }
+                            noValidate
+                            sx={ { mt: 1 } }
+                        >
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                autoComplete="email"
+                                autoFocus
+                                value={ formData.email }
+                                onChange={ handleChange }
+                                error={ !! errors.email }
+                                helperText={ errors.email }
+                            />
+                            <TextField
+                                disabled
+                                margin="normal"
+                                fullWidth
+                                name="password"
+                                label="Password (not implemented)"
+                                type="password"
+                                id="password"
+                                autoComplete="current-password"
+                            />
+                            { /* <FormControlLabel
+                                control={
+                                    <Checkbox value="remember" color="primary" />
+                                }
+                                label="Remember me"
+                            /> */ }
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                sx={ { mt: 3, mb: 2 } }
+                            >
+                                Sign In
+                            </Button>
+                            <Grid container>
+                                { /* <Grid item xs>
+                                    <Link href="#" variant="body2">
+                                        Forgot password?
+                                    </Link>
+                                </Grid> */ }
+                                <Grid item>
+                                    <Link href="/signup" variant="body2">
+                                        Don't have an account? Sign Up
+                                    </Link>
+                                </Grid>
+                            </Grid>
+                        </Box>
+                    </Box>
+            }
+        </Container>
     )
 }
