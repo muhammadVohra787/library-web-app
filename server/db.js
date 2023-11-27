006bf3092e2d50d10bb900d47afc810cc31890b0
Bjornar Egede-Nissen <gallopping_gryphon@outlook.com>
2023-11-27 00:57:48 -0500

    Formatting

--- server/db.js
+++ server/db.js
@@ -1,26 +1,29 @@
-import mongoose from "mongoose";
-import config from "./config/config.js";
+import mongoose from 'mongoose'
+import config from './config/config.js'
 
-import addBooksToDatabase from './Initializers/books.initializer.js';
+import addBooksToDatabase from './Initializers/books.initializer.js'
 
 export const connectDB = async() => {
     try {
-        await mongoose.connect(config.mongoUri, {
+        await mongoose.connect( config.mongoUri, {
             useNewUrlParser: true,
             // useCreateIndex: true,
             useUnifiedTopology: true,
-        })
-        await addBooksToDatabase();
-        console.log("Connected to the database!");
-    }catch (error) {
-        console.log("unable to connect to database: ${config.mongoUri}");
+        } )
+        await addBooksToDatabase()
+        console.log( 'Connected to the database!' )
     }
-};
+    catch ( error ) {
+        console.log( 'unable to connect to database: ${config.mongoUri}' )
+    }
+}
 
 export const closeDBConnection = async() => {
     try {
-        await mongoose.connection.close();
-        console.log('Close DB connection successful.');
-    } catch (error) {
-        console.error(error);
-    }};
+        await mongoose.connection.close()
+        console.log( 'Close DB connection successful.' )
+    }
+    catch ( error ) {
+        console.error( error )
+    }
+}
