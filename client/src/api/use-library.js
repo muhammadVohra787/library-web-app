2afc7d702401a430575db3f5849f302f281f0cca
Bjornar Egede-Nissen <gallopping_gryphon@outlook.com>
2023-11-27 18:15:09 -0500

    Track checkout status better

--- client/src/api/use-library.js
+++ client/src/api/use-library.js
@@ -6,6 +6,7 @@ export default function useLibrary( userId ) {
     const loanHistory = useFetch()
     const loanStatus = useFetch()
     const loanControl = useFetch()
+    const bookCheckouts = useFetch()
 
     useEffect( () => {
         if ( loanControl.isComplete ) {
@@ -15,14 +16,17 @@ export default function useLibrary( userId ) {
 
     useEffect( () => {
         if ( loanControl.isComplete ) {
+            // Refetch loan data (fetch is only executed if it has already been used)
+            loanStatus.refetch()
             loanHistory.refetch()
+            bookCheckouts.refetch()
         }
     }, [ loanControl.isComplete ] )
 
     useEffect( () => {
         if ( loanStatus.isComplete ) {
-            // console.log( 'loanDetails.data.returnDate ', ! loanStatus.data.returnDate, !! loanStatus.data.returnDate )
-            setIsCheckedOutByUser( !! loanStatus.data.length )
+            const loanItem = loanStatus.data.length ? loanStatus.data[ 0 ] : null
+            setIsCheckedOutByUser( !! loanItem )
         }
     }, [ loanStatus.isComplete ] )
 
@@ -35,22 +39,43 @@ export default function useLibrary( userId ) {
         get isCheckedOutByUser() {
             return isCheckedOutByUser
         },
-
+        get bookCheckouts() {
+            return bookCheckouts.isComplete ? bookCheckouts.data : []
+        },
+        get bookCheckoutCount() {
+            return bookCheckouts.isComplete ? bookCheckouts.data.length : -1
+        },
+        get checkoutStatusChanged() {
+            console.log( 'loanControl.data', loanControl.data )
+            return loanControl.isComplete && loanControl.data && loanControl.data._id ? ! loanControl.data.returnDate : null
+        },
+        get isCheckoutStatusChangePending() {
+            return loanControl.isFetching
+        },
+        get isCheckoutStatusCheckPending() {
+            return loanStatus.isFetching
+        },
+        getBookCheckouts( bookId ) {
+            bookCheckouts.fetch( '/loans', { query: { bookId, isReturned: false } } )
+        },
         getLoans() {
             loanHistory.fetch( '/loans', { query: { userId } } )
         },
         getBorrowStatus( bookId ) {
-            loanStatus.fetch( `/loans`, { query: { userId, bookId } } )
+            console.log( 'getBorrowStatus\n\n', { userId, bookId, isReturned: false } )
+            loanStatus.fetch( `/loans`, { query: { userId, bookId, isReturned: false } } )
         },
         borrow( bookId ) {
+            console.log( 'BORROW: ', userId )
             const dueDate = new Date()
             const loanData = {
                 bookId,
                 userId,
                 loanDate: Date.now(),
                 dueDate: new Date().setDate( dueDate.getDate() + 14 ),
-                //
             }
+
+            console.log( 'Borrowing book: ', loanData )
 
             const options = {
                 method: 'POST',
