70a6011782fcf7ceb5cab7df14bf09efe3ec9a9f
Bjornar Egede-Nissen <gallopping_gryphon@outlook.com>
2023-11-27 01:00:41 -0500

    Re-added importBooks script

--- server/scripts/import-books.js
+++ server/scripts/import-books.js
@@ -1,34 +1,39 @@
-import fs from 'fs';
-import Book from '../models/book.model.js';
-import { connectDB, closeDBConnection } from '../db.js';
-import { fileURLToPath } from 'url';
-import { join, dirname } from 'path';
+// import fs from 'fs'
+import Book from '../models/book.model.js'
+import { connectDB, closeDBConnection } from '../db.js'
+// import { fileURLToPath } from 'url'
+// import { join, dirname } from 'path'
+import sampleData from './sample-data.js'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// const __filename = fileURLToPath( import.meta.url )
+// const __dirname = dirname( __filename )
 
-connectDB();
+connectDB()
 
-const jsonFilePath = join(__dirname, '../public/books.js');
-console.log(jsonFilePath);
-const jsonContent = JSON.parse(fs.readFileSync(jsonFilePath, 'utf-8'));
-
-const importBooks = async () => {
+/**
+ * Script originally by @Julian0718
+ *
+ * @link https://github.com/galloppinggryphon/library-web-app/commit/a13e27bd3bcb8b44b26288170460b8b7f1678a01#diff-d9d62cfbcdf55353610e4e317816576f12beb7ebcf27ddf9c95c155b9125c54f
+ */
+const importBooks = async() => {
     try {
-        for (const book of jsonContent) {
-          const existingBook = await Book.findOne({ title: book.title });
-          if (!existingBook) {
-            await Book.create(book);
-            console.log(`Book imported: ${book.title}`);
-          } else {
-            console.log(`Book already exist: ${book.title}. Not imported.`);
-          }
+        for ( const book of sampleData ) {
+            const existingBook = await Book.findOne( { title: book.title } )
+            if ( ! existingBook ) {
+                await Book.create( book )
+                console.log( `Book imported: ${book.title}` )
+            }
+            else {
+                console.log( `Book already exist: ${book.title}. Not imported.` )
+            }
         }
-      } catch (error) {
-        console.error(error);
-      } finally {
-        closeDBConnection();
     }
-  };
+    catch ( error ) {
+        console.error( error )
+    }
+    finally {
+        closeDBConnection()
+    }
+}
 
-  importBooks();
+importBooks()
