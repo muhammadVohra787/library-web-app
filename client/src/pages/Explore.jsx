2d7c6c18ed6305c660169bfecc03ceb3c9356a07
Bjornar Egede-Nissen <gallopping_gryphon@outlook.com>
2023-11-27 18:42:55 -0500

    Improved loading UX

--- client/src/pages/Explore.jsx
+++ client/src/pages/Explore.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useLayoutEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import {
     Autocomplete,
     Container,
@@ -31,9 +31,10 @@ const ExplorePage = () => {
 
     const bookRequest = useBookData()
 
-    useLayoutEffect( () => {
+    useEffect( () => {
         bookRequest.getBooks( { sortBy, sortOrder } )
-        console.log( 'bookRequest', bookRequest )
+        console.log( 'bookRequest', bookRequest, { sortBy, sortOrder, searchValue } )
+        setIsLoading( true )
     }, [ sortBy, sortOrder, searchValue ] )
 
     console.log( ' bookRequest.status.isFetching', bookRequest.status.isFetching )
@@ -57,23 +58,21 @@ const ExplorePage = () => {
 
     return (
         <Container>
-            <Box>
+            <Container>
                 <h1>Explore</h1>
-            </Box>
-            {
-                isLoading && <CircularProgress />
-            }
+            </Container>
             {
                 bookRequest.status.isError &&
                     <Paper><Box p={ 3 }>Book not found.</Box></Paper>
             }
             {
-                ! isLoading && ! bookRequest.status.isError && Array.isArray( bookRequest.data ) && <>
+                ! bookRequest.status.isError && Array.isArray( bookRequest.data ) && <>
                     <Container>
                         <Stack direction="row" alignItems='center' mb={ 5 }>
                             <Autocomplete
                                 style={ { flex: 1 } }
                                 disablePortal
+                                disabled={ isLoading }
                                 id="combo-box-demo"
                                 options={ options }
                                 sx={ { width: 400 } }
@@ -87,7 +86,7 @@ const ExplorePage = () => {
                                     />
                                 ) }
                             />
-                            <Button onClick={ handleSortToggle } sx={ { marginLeft: '20px' } }>
+                            <Button disabled={ isLoading } onClick={ handleSortToggle } sx={ { marginLeft: '20px' } }>
                                 { sortOrder === 'asc' ? 'Sort Descending' : 'Sort Ascending' }
                             </Button>
                             <FormControl sx={ { marginLeft: '10px', minWidth: '120px' } }>
@@ -98,6 +97,7 @@ const ExplorePage = () => {
                                     value={ sortBy }
                                     label="Sort Type"
                                     onChange={ handleSortTypeChange }
+                                    disabled={ isLoading }
                                 >
                                     <MenuItem value="title">Title</MenuItem>
                                     <MenuItem value="author">Author</MenuItem>
@@ -105,56 +105,62 @@ const ExplorePage = () => {
                             </FormControl>
                         </Stack>
                     </Container>
-                    <Container>
-                        <Grid
-                            container
-                            spacing={ 4 }
-                            columnSpacing={ { xs: 1, sm: 2, md: 3 } }
-                        >
-                            { bookRequest.data.map( ( item, index ) => (
-                                <Grid item xs={ 12 } sm={ 6 } md={ 4 } key={ index }>
-                                    <Paper
-                                        className="bookBlock"
-                                        elevation={ 3 }
-                                        style={ {
-                                            padding: '20px',
-                                            borderRadius: '10px',
-                                        } }
-                                    >
-                                        <Typography variant="h6" className="titleBook">
-                                            { item.title }
-                                        </Typography>
-                                        <NavLink to={ `/book/${ item.slug}` }>
-                                            <img
-                                                height={ 400 }
-                                                width={ 180 }
-                                                src={ imageFolder + item.thumbnail }
-                                                alt="bookImage"
+                    {
+                        isLoading && <Container><CircularProgress /></Container>
+                    }
+                    {
+                        ! isLoading &&
+                            <Container>
+                                <Grid
+                                    container
+                                    spacing={ 4 }
+                                    columnSpacing={ { xs: 1, sm: 2, md: 3 } }
+                                >
+                                    { bookRequest.data.map( ( item, index ) => (
+                                        <Grid item xs={ 12 } sm={ 6 } md={ 4 } key={ index }>
+                                            <Paper
+                                                className="bookBlock"
+                                                elevation={ 3 }
                                                 style={ {
-                                                    display: 'block',
-                                                    margin: 'auto',
-                                                    marginTop: '15px',
-                                                    marginBottom: '15px',
+                                                    padding: '20px',
+                                                    borderRadius: '10px',
                                                 } }
-                                            />
-                                        </NavLink>
-                                        <Typography
-                                            variant="body2"
-                                            className="bookAuthor"
-                                        >
-                                            Author: { item.author }
-                                        </Typography>
-                                        <Typography
-                                            variant="body2"
-                                            className="bookStock"
-                                        >
-                                            Available: { item.stock }
-                                        </Typography>
-                                    </Paper>
+                                            >
+                                                <Typography variant="h6" className="titleBook">
+                                                    { item.title }
+                                                </Typography>
+                                                <NavLink to={ `/book/${ item.slug}` }>
+                                                    <img
+                                                        height={ 400 }
+                                                        width={ 180 }
+                                                        src={ imageFolder + item.thumbnail }
+                                                        alt="bookImage"
+                                                        style={ {
+                                                            display: 'block',
+                                                            margin: 'auto',
+                                                            marginTop: '15px',
+                                                            marginBottom: '15px',
+                                                        } }
+                                                    />
+                                                </NavLink>
+                                                <Typography
+                                                    variant="body2"
+                                                    className="bookAuthor"
+                                                >
+                                                    Author: { item.author }
+                                                </Typography>
+                                                <Typography
+                                                    variant="body2"
+                                                    className="bookStock"
+                                                >
+                                                    Available: { item.stock }
+                                                </Typography>
+                                            </Paper>
+                                        </Grid>
+                                    ) ) }
                                 </Grid>
-                            ) ) }
-                        </Grid>
-                    </Container>
+                            </Container>
+                    }
                 </>
             }
         </Container>
