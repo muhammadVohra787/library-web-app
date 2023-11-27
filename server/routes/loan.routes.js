e4fc608c98d77c45843645b2095e01f78c6b22bd
Bjornar Egede-Nissen <gallopping_gryphon@outlook.com>
2023-11-27 01:44:25 -0500

    Formatting

--- server/routes/loan.routes.js
+++ server/routes/loan.routes.js
@@ -1,12 +1,12 @@
-import express from 'express';
-import { getLoans, getLoanByID, createLoan, updateLoan, deleteLoan } from '../controllers/loan.controller.js';
+import express from 'express'
+import { getLoans, getLoanByID, createLoan, updateLoan, deleteLoan } from '../controllers/loan.controller.js'
 
-const router = express.Router();
+const router = express.Router()
 
-router.get('/loans', getLoans);
-router.get('/loans/:loanid', getLoanByID);
-router.post('/loans', createLoan);
-router.put('/loans/:loanid', updateLoan);
-router.delete('/loans/:loanid', deleteLoan);
+router.get( '/loans', getLoans )
+router.get( '/loans/:loanid', getLoanByID )
+router.post( '/loans', createLoan )
+router.put( '/loans/:loanid', updateLoan )
+router.delete( '/loans/:loanid', deleteLoan )
 
-export default router;
+export default router
