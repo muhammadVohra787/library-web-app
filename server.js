99b4fdeeae346e5e323af9c5a3cc1fba174be4ea
Bjornar Egede-Nissen <gallopping_gryphon@outlook.com>
2023-11-27 21:23:42 -0500

    Revert

--- server.js
+++ server.js
@@ -1,26 +1,18 @@
-import express from 'express';
-import config from './server/config/config.js';
-import { connectDB } from './server/db.js';
-import booksRouter from './server/routes/books.routes.js'; // Adjust this path if necessary
-
-// Create an instance of express
-const app = express();
-
-// Middleware to parse JSON bodies
+/**
+ * App Entrypoint
+ */
+//server.js
+import config from "./server/config/config.js";
+import { connectDB } from "./server/db.js";
+import app from "./server/express.js";
+import express from "express";
 app.use(express.json());
 
-// Connect to the database
 connectDB();
-
-// Default route for the server
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     res.json({ message: "Welcome to this application." });
 });
 
-// Books routes
-app.use('/books', booksRouter);
-
-// Start the server
 app.listen(config.port, (err) => {
     if (err) {
         console.log(err);
@@ -28,6 +20,3 @@ app.listen(config.port, (err) => {
     console.info("Server started on port %s.", config.port);
     console.info("Waiting for DB connection...");
 });
-
-// Export the express app (optional, useful for testing)
-export default app;
