b6cc68bff99a154701e4b3fd748a8e88a1bcfe14
Bjornar Egede-Nissen <gallopping_gryphon@outlook.com>
2023-11-27 20:36:43 -0500

    Formatting

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,7 +1,7 @@
-//user.controller.js
+// user.controller.js
 import User from '../models/user.model.js'
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 export const getUsers = async( req, res ) => {
     const { email } = req.query
 
@@ -23,28 +23,29 @@ export const getUserByID = async( req, res ) => {
     res.json( user )
 }
 
-export const createUser = async (req, res) => {
-    const { name, email, password } = req.body;
+export const createUser = async( req, res ) => {
+    const { name, email, password } = req.body
 
     try {
         // Hash the password before saving it to the database
-        const hashedPassword = await bcrypt.hash(password, 10);
+        const hashedPassword = await bcrypt.hash( password, 10 )
 
-        const newUser = new User({
+        const newUser = new User( {
             name,
             email,
             password: hashedPassword, // Save the hashed password
-        });
+        } )
 
-        const userSaved = await newUser.save();
-        res.json(userSaved);
-        console.log('user-reached controller');
-    } catch (error) {
-        console.log(error);
-        console.log('user-reached controller- Unsuccessful');
-        res.status(500).json({ error: error.message });
+        const userSaved = await newUser.save()
+        res.json( userSaved )
+        console.log( 'user-reached controller' )
     }
-};
+    catch ( error ) {
+        console.log( error )
+        console.log( 'user-reached controller- Unsuccessful' )
+        res.status( 500 ).json( { error: error.message } )
+    }
+}
 export const updateUser = async( req, res ) => {
     const user = await User.findByIdAndUpdate( req.params.userid, req.body, {
         new: true,
@@ -63,30 +64,29 @@ export const deleteUser = async( req, res ) => {
     res.json( user )
 }
 
-
-
-export const signIn = async (req, res) => {
-    const { email, password } = req.body;
+export const signIn = async( req, res ) => {
+    const { email, password } = req.body
 
     try {
-        const user = await User.findOne({ email });
+        const user = await User.findOne( { email } )
 
-        if (!user) {
-            return res.status(401).json({ error: 'Invalid email' });
+        if ( ! user ) {
+            return res.status( 401 ).json( { error: 'Invalid email' } )
         }
 
-        const isPasswordValid = await bcrypt.compare(password, user.password);
+        const isPasswordValid = await bcrypt.compare( password, user.password )
 
-        if (!isPasswordValid) {
-            return res.status(401).json({ error: 'Invalid password' });
+        if ( ! isPasswordValid ) {
+            return res.status( 401 ).json( { error: 'Invalid password' } )
         }
-        const token = jwt.sign({ userId: user._id }, 'your-secret-key', {
+        const token = jwt.sign( { userId: user._id }, 'your-secret-key', {
             expiresIn: '1h',
-        });
+        } )
 
-        res.json({ success: true, token });
-    } catch (error) {
-        console.error('Error signing in:', error);
-        res.status(500).json({ success: false, error: 'Internal Server Error' });
+        res.json( { success: true, token } )
     }
-};
+    catch ( error ) {
+        console.error( 'Error signing in:', error )
+        res.status( 500 ).json( { success: false, error: 'Internal Server Error' } )
+    }
+}
