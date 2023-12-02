7169257eb2f7cabbe0964ffd80e4f98afa7d6aa0
muhammadVohra787 <mvohra10@my.centennialcollege.ca>
2023-12-01 22:07:58 -0500

    Theres duplicated code for signUp function. Search.

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,27 +1,23 @@
-// auth.controller.js
-import User from '../models/user.model.js'
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
+// // auth.controller.js
+// import User from '../models/user.model.js'
+// import bcrypt from 'bcrypt';
+// import jwt from 'jsonwebtoken';
 
-export const signIn = async (req, res) => {
-    const { email, password } = req.body;
+// export const signIn = async (req, res) => {
+//     const { email, password } = req.body;
 
-    try {
-        const user = await User.findOne({ email });
+//     try {
+//         const user = await User.findOne({ email });
+//         console.log( 'signin Reached -auth.controller' )
+//         if (user && !isPasswordValid) {
+//             return res.status(401).json({ error: 'Invalid credentials' });
+//         }
+//         const isPasswordValid = await bcrypt.compare(password, user.password);
 
-        if (!user) {
-            return res.status(401).json({ error: 'Invalid credentials' });
-        }
-        const isPasswordValid = await bcrypt.compare(password, user.password);
-
-        if (!isPasswordValid) {
-            return res.status(401).json({ error: 'Invalid credentials' });
-        }
-
-        // Generate and send a response upon successful authentication
-        res.json({ success: true, user });
-    } catch (error) {
-        console.error('Error signing in:', error);
-        res.status(500).json({ success: false, error: 'Internal Server Error' });
-    }
-};
+//         // Generate and send a response upon successful authentication
+//         res.json({ success: true, user });
+//     } catch (error) {
+//         console.error('Error signing in:', error);
+//         res.status(500).json({ success: false, error: 'Internal Server Error' });
+//     }
+// };
