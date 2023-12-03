b2bdd9623afc6c9fbab48e15bce237b888467e80
muhammadVohra787 <mvohra10@my.centennialcollege.ca>
2023-12-03 00:41:06 -0500

    user password and email being check here now

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,23 +1,34 @@
-// // auth.controller.js
-// import User from '../models/user.model.js'
-// import bcrypt from 'bcrypt';
-// import jwt from 'jsonwebtoken';
+//server/controller/auth.controller
+import User from "../models/user.model.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
-// export const signIn = async (req, res) => {
-//     const { email, password } = req.body;
+export const signIn = async (req, res) => {
+    const { email, password } = req.body;
+    console.log("auth-controller Reached");
+    try {
+        const user = await User.findOne({ email });
+        console.log("signin Reached -auth.controller");
+        const passwordsMatch = await bcrypt.compare(password, user.password);
+        if (!user || !passwordsMatch) {
+            return res.status(401).json({ error: "Invalid credentials" });
+        }
 
-//     try {
-//         const user = await User.findOne({ email });
-//         console.log( 'signin Reached -auth.controller' )
-//         if (user && !isPasswordValid) {
-//             return res.status(401).json({ error: 'Invalid credentials' });
-//         }
-//         const isPasswordValid = await bcrypt.compare(password, user.password);
+        const token = jwt.sign(
+            {
+                userId: user._id,
+                userEmail: user.email,
+            },
+            "RANDOM-TOKEN",
+            { expiresIn: "24h" }
+        );
 
-//         // Generate and send a response upon successful authentication
-//         res.json({ success: true, user });
-//     } catch (error) {
-//         console.error('Error signing in:', error);
-//         res.status(500).json({ success: false, error: 'Internal Server Error' });
-//     }
-// };
+        res.status(200).json({ success: true, user, token });
+    } catch (error) {
+        console.error("Error signing in:", error);
+        res.status(500).json({
+            success: false,
+            error: "x1Internal Server Error",
+        });
+    }
+};
