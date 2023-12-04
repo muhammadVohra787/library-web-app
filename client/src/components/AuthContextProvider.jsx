a5a0c3b1c69ebe3378a26968334cba3cb3700afe
Bjornar Egede-Nissen <gallopping_gryphon@outlook.com>
2023-12-04 17:38:25 -0500

    Formatting

--- client/src/components/AuthContextProvider.jsx
+++ client/src/components/AuthContextProvider.jsx
@@ -1,4 +1,4 @@
-//src/compo/authcontextProvider
+// src/compo/authcontextProvider
 import AuthContext from '@/api/auth-context'
 import { useLocalStorage } from '@/api/local-storage'
 import useAuthentication from '../api/use-authentication'
@@ -6,18 +6,19 @@ import SessionTimeoutWarningModal from './SessionOut'
 
 // import jwt from 'jsonwebtoken';
 export function AuthContextProvider( { children } ) {
-
     const [ persistedUserId, setPersistedUserId ] = useLocalStorage( 'userId', '' )
     const [ persistedToken, setPersistedToken ] = useLocalStorage( 'token', '' )
     const auth = useAuthentication()
-    const expiryDate = new Date( persistedToken.exp * 1000)
+    const expiryDate = new Date( persistedToken.exp * 1000 )
+
     const checkToken = () => {
-        if (!persistedToken || new Date()> expiryDate) {
-            auth.sessionSignOut();
-            return false;
+        if ( ! persistedToken || new Date() > expiryDate ) {
+            auth.sessionSignOut()
+            return false
         }
-        console.log('Token is available');
-        return true;
-     }
+        console.log( 'Token is available' )
+        return true
+    }
+
     return <AuthContext.Provider value={ { userId: persistedUserId, checkToken, setToken: setPersistedToken, setUserId: setPersistedUserId } }>{ children }</AuthContext.Provider>
 }
