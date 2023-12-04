0e7992d725dc293f20e5930b6ba341aaf74b6b5b
muhammadVohra787 <mvohra10@my.centennialcollege.ca>
2023-12-04 16:56:57 -0500

    validates token, signs out

--- client/src/components/AuthContextProvider.jsx
+++ client/src/components/AuthContextProvider.jsx
@@ -1,17 +1,23 @@
+//src/compo/authcontextProvider
 import AuthContext from '@/api/auth-context'
 import { useLocalStorage } from '@/api/local-storage'
+import useAuthentication from '../api/use-authentication'
+import SessionTimeoutWarningModal from './SessionOut'
 
+// import jwt from 'jsonwebtoken';
 export function AuthContextProvider( { children } ) {
+
     const [ persistedUserId, setPersistedUserId ] = useLocalStorage( 'userId', '' )
     const [ persistedToken, setPersistedToken ] = useLocalStorage( 'token', '' )
-
+    const auth = useAuthentication()
+    const expiryDate = new Date( persistedToken.exp * 1000)
     const checkToken = () => {
-        // Check token validity here
-        // Check persistedToken and userId
-        // Invalidate token with setPersistedToken(null), setUserId(null)
-
-        return !! persistedUserId
-    }
-
+        if (!persistedToken || new Date()> expiryDate) {
+            auth.sessionSignOut();
+            return false;
+        }
+        console.log('Token is available');
+        return true;
+     }
     return <AuthContext.Provider value={ { userId: persistedUserId, checkToken, setToken: setPersistedToken, setUserId: setPersistedUserId } }>{ children }</AuthContext.Provider>
 }
