6fde5762ba2413d7659209ef42a99b01ce10b763
Bjornar Egede-Nissen <gallopping_gryphon@outlook.com>
2023-12-05 12:28:37 -0500

    Formatting

--- server/config/config.js
+++ server/config/config.js
@@ -1,15 +1,15 @@
-const databaseName = process.env.MONGO_DB_NAME || "Library";
+const databaseName = process.env.MONGO_DB_NAME || 'Library'
 
 const config = {
     env: process.env.NODE_ENV,
-    port: process.env.PORT || "3000",
-    jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
+    port: process.env.PORT || '3000',
+    jwtSecret: process.env.JWT_SECRET || 'YOUR_secret_key',
     mongoDbName: databaseName,
     mongoUri:
         process.env.MONGO_URI ||
-        `mongodb://${process.env.IP || "127.0.0.1"}:${
-            process.env.MONGO_PORT || "27017"
+        `mongodb://${process.env.IP || '127.0.0.1'}:${
+            process.env.MONGO_PORT || '27017'
         }/${databaseName}`,
-};
+}
 
-export default config;
+export default config
