2569e2b0c77929128aa3b4fb08e1f367ff4ba557
Bjornar Egede-Nissen <gallopping_gryphon@outlook.com>
2023-12-04 19:06:15 -0500

    Cleanup

--- client/src/components/AuthContextProvider.jsx
+++ client/src/components/AuthContextProvider.jsx
@@ -1,14 +1,10 @@
 // src/compo/authcontextProvider
 import AuthContext from '@/api/auth-context'
 import { useLocalStorage } from '@/api/local-storage'
-import useAuthentication from '../api/use-authentication'
-import SessionTimeoutWarningModal from './SessionOut'
 
-// import jwt from 'jsonwebtoken';
 export function AuthContextProvider( { children } ) {
     const [ persistedUserId, setPersistedUserId ] = useLocalStorage( 'userId', '' )
     const [ persistedToken, setPersistedToken ] = useLocalStorage( 'token', '' )
-    // const auth = useAuthentication()
 
     return <AuthContext.Provider value={ {
         userId: persistedUserId,
