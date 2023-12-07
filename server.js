9378424377579c273856c4c7d5045cec8ad11690
Bjornar Egede-Nissen <gallopping_gryphon@outlook.com>
2023-12-07 13:09:50 -0500

    Diff startup procedures for prod and dev

--- server.js
+++ server.js
@@ -1,22 +1,49 @@
 /**
  * App Entrypoint
  */
-//server.js
-import config from "./server/config/config.js";
-import { connectDB } from "./server/db.js";
-import app from "./server/express.js";
-import express from "express";
-app.use(express.json());
+// server.js
+import config from './server/config/config.js'
+import { connectDB } from './server/db.js'
+import app from './server/express.js'
+import express from 'express'
 
-connectDB();
-app.get("/", (req, res) => {
-    res.json({ message: "Welcome to this application." });
-});
+const stage = process.env.STAGE && process.env.STAGE.toUpperCase()
 
-app.listen(config.port, (err) => {
-    if (err) {
-        console.log(err);
+console.log( 'Starting server...' )
+console.log( `STAGE=${stage}` )
+console.log( `NODE_ENV=${process.env.NODE_ENV}` )
+
+if ( 'STAGE' in process.env ) {
+    const hasMongoConf = process.env.JWT_SECRET && process.env.MONGO_URI
+
+    if ( stage === 'PRODUCTION' && ! hasMongoConf ) {
+        console.log( '\nSERVER DID NOT START' )
+        console.log( '\nServer is set to PRODUCTION but is missing configuration variables for JWT_SECRET and/or MONGO_URI.' )
     }
-    console.info("Server started on port %s.", config.port);
-    console.info("Waiting for DB connection...");
-});
+    else {
+        main()
+    }
+}
+else {
+    console.log( '\nSERVER DID NOT START' )
+    console.log( '\nThe server has not been configured yet.\n' )
+    console.log( 'In production mode, add NODE_ENV, STAGE, JWT_SECRET and MONGO_URI.' )
+    console.log( 'In development mode, STAGE=development is required, others are optional.' )
+}
+
+function main() {
+    app.use( express.json() )
+
+    connectDB()
+    app.get( '/', ( req, res ) => {
+        res.json( { message: 'Welcome to this application.' } )
+    } )
+
+    app.listen( config.port, ( err ) => {
+        if ( err ) {
+            console.log( err )
+        }
+        console.info( 'Server started on port %s.', config.port )
+        console.info( 'Waiting for DB connection...' )
+    } )
+}
