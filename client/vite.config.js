b79d3fe940cf80ff3385969db38827cddef267ba
Bjornar Egede-Nissen <gallopping_gryphon@outlook.com>
2023-12-07 14:40:12 -0500

    Added port forwarding

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react-swc'
 import alias from '@rollup/plugin-alias'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import dotenv from 'dotenv'
+const { parsed: env } = dotenv.config()
+
+// eslint-disable-next-line no-undef
+const { PORT = 3000 } = env
 
 const __filename = fileURLToPath( import.meta.url )
 const __dirname = path.dirname( __filename )
@@ -11,6 +16,19 @@ const __dirname = path.dirname( __filename )
 export default defineConfig( {
     build: {
         outDir: '../client-dist/app',
+    },
+    server: {
+        proxy: {
+            '/api': {
+                target: `http://localhost:${PORT}`,
+                changeOrigin: true,
+            },
+            '/auth': {
+                target: `http://localhost:${PORT}`,
+                changeOrigin: true,
+            },
+
+        },
     },
     plugins: [
         react(),
