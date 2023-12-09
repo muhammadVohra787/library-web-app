2d9c03cd73e192dedbba41449f4c4b2c5107fe6b
muhammadVohra787 <mvohra10@my.centennialcollege.ca>
2023-12-09 04:45:48 -0500

    s

--- server/db.js
+++ server/db.js
@@ -5,7 +5,7 @@ import importBooks from './scripts/import-books.js'
 export const connectDB = async() => {
     try {
         console.info( 'Waiting for DB connection...' )
-        await mongoose.connect( process.env.mongoUri, {
+        await mongoose.connect( config.mongoUri, {
             useNewUrlParser: true,
             // useCreateIndex: true,
             useUnifiedTopology: true,
