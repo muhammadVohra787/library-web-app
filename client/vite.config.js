0e550144d3d18c567b919f29b077dca9093e4dd7
muhammadVohra787 <mvohra10@my.centennialcollege.ca>
2023-12-09 14:32:03 -0500

    config updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,51 +1,55 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import alias from '@rollup/plugin-alias'
-import path from 'path'
-import fs from 'fs'
-import { fileURLToPath } from 'url'
-import dotenv from 'dotenv'
-import config from '../config/config'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import alias from "@rollup/plugin-alias";
+import path from "path";
+import fs from "fs";
+import { fileURLToPath } from "url";
+import dotenv from "dotenv";
+import config from "../config/config";
 
 // Use root .env
 // const env = fs.existsSync( '../.env' ) ? dotenv.config( { path: '../.env' } ).parsed : {}
-const { PORT = 3000, apiEndpointUrl = '/api' } = config
+const { PORT = 3000, apiEndpointUrl = "/api" } = config;
 
-const __filename = fileURLToPath( import.meta.url )
-const __dirname = path.dirname( __filename )
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
-export default defineConfig( {
+export default defineConfig({
     // base: './',l
 
     build: {
-        outDir: '../client-dist/app',
+        outDir: "../client-dist/app",
     },
     // Replace variables on build
     define: {
-        API_ENDPOINT_URL: JSON.stringify( apiEndpointUrl ),
+        API_ENDPOINT_URL: JSON.stringify(apiEndpointUrl),
     },
     server: {
         proxy: {
-            '/api': {
-                target: `https://library-web-app-one.vercel.app/`,
+            "/api": {
+                target: `http://localhost:${PORT}`,
                 changeOrigin: true,
-                secure: false,
             },
-            '/auth': {
-                target: `https://library-web-app-one.vercel.app/api/`,
+            "/auth": {
+                target: `http://localhost:${PORT}`,
                 changeOrigin: true,
-                secure: false,
             },
-
         },
+        rewrites: [
+            {
+                source: "/api/:path(.*)",
+                destination: `http://localhost:${PORT}`,
+            },
+            { source: "/(.*)", destination: "/index.html" },
+        ],
     },
     plugins: [
         react(),
-        alias( {
+        alias({
             entries: [
-                { find: '@', replacement: path.resolve( __dirname, 'src' ) },
+                { find: "@", replacement: path.resolve(__dirname, "src") },
             ],
-        } ),
+        }),
     ],
-} )
+});
