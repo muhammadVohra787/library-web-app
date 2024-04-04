98ab6d7d4fd3f8e9de2f1e544342050707a465c5
Muhammad Vohra <144752461+muhammadVohra787@users.noreply.github.com>
2024-04-04 05:15:38 -0400

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,26 @@ import { AuthContextProvider } from './components/AuthContextProvider'
 // import { AuthProvider } from "./api/auth-context";
 
 function App() {
+    const DummyFetch = () => {
+        useEffect(() => {
+            const pingServer = async () => {
+                try {
+                    const response = await fetch('/ping');
+                    const data = await response.text();
+                    console.log(data);
+                } catch (error) {
+                    console.error('Error fetching:', error);
+                }
+            };
+
+            pingServer();
+            const interval = setInterval(pingServer, 10 * 60 * 1000);
+
+            return () => clearInterval(interval);
+        }, []);
+
+        return null;
+    };
     return (
         <AuthContextProvider>
             <Header />
